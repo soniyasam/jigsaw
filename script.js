8c458bbb0ebce1d8f165eb959d69d8af8d595d86
soniyasam <61118463+soniyasam@users.noreply.github.com>
2021-05-01 00:31:50 +0530

    new image updated

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ var clockInterval;
 function init() {
     img = new Image();
     img.addEventListener('load', onImage, false);
-    img.src = "mcymlogo.png";
+    img.src = "mcym_logo.png";
 }
 
 function onImage(e) {
